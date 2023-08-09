73fbed1aaea4b0f853c4ba54beb83bd29b795425
timdegen <104884878+timdegen@users.noreply.github.com>
2023-08-09 23:45:45 +0100

    Add aptos models

--- blockchain/src/aptos/aptos_model.rs
+++ blockchain/src/aptos/aptos_model.rs
@@ -2,17 +2,46 @@ use typeshare::typeshare;
 //use serde::{Serialize, Deserialize};
 
 #[typeshare]
+#[allow(dead_code)]
 struct AptosResource<T> {
     r#type: String,
     data: T
 }
 
 #[typeshare]
+#[allow(dead_code)]
 struct AptosResourceBalance {
     coin: AptosResourceCoin
 }
 
 #[typeshare]
+#[allow(dead_code)]
 struct AptosResourceCoin {
     value: String
+}
+
+#[typeshare]
+#[allow(dead_code)]
+struct AptosAccount {
+    sequence_number: String
+}
+
+
+#[typeshare]
+#[allow(dead_code)]
+struct AptosTransaction {
+    success: bool
+}
+
+#[typeshare]
+#[allow(dead_code)]
+struct AptosTransactionBroacast {
+    hash: String
+}
+
+#[typeshare]
+#[allow(dead_code)]
+struct AptosGasFee {
+    gas_estimate: i32,
+    prioritized_gas_estimate: i32,
 }
