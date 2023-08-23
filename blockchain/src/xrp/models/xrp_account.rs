7ca7e1ccfc8f244623e5860db39881405dcc4f3c
timdegen <104884878+timdegen@users.noreply.github.com>
2023-08-23 23:21:50 +0100

    Add xrp models

---
+++ blockchain/src/xrp/models/xrp_account.rs
@@ -0,0 +1,40 @@
+#[typeshare]
+struct XRPResult<T> {
+    result: T
+}
+
+#[typeshare]
+struct XRPAccountResult {
+    account_data: XRPAccount,
+    ledger_current_index: i32,
+}
+
+#[typeshare]
+struct XRPAccount {
+    #[serde(rename = "Balance")]
+    balance: String,
+    #[serde(rename = "Sequence")]
+    sequence: i32,
+}
+
+#[typeshare]
+struct XRPFee {
+    drops: XRPDrops,
+}
+
+#[typeshare]
+struct XRPDrops {
+    median_fee: String,
+}
+
+#[typeshare]
+struct XRPTransactionBroadcast {
+    accepted: bool,
+    engine_result_message: Option<String>,
+    tx_json: Option<XRPTransaction>,
+}
+
+#[typeshare]
+struct XRPTransaction {
+    hash: String,
+}
