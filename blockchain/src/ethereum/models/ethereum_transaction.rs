5a5a927b5ec4ea67537cb202dd359b9c13a3c486
timdegen <104884878+timdegen@users.noreply.github.com>
2023-08-31 05:09:14 +0100

    Add EthereumFeeHistory

--- blockchain/src/ethereum/models/ethereum_transaction.rs
+++ blockchain/src/ethereum/models/ethereum_transaction.rs
@@ -2,3 +2,10 @@
 struct EthereumTransaction {
     status: String,
 }
+
+#[typeshare]
+struct EthereumFeeHistory {
+    reward: Vec<Vec<String>>,
+    #[serde(rename = "baseFeePerGas")]
+    base_fee_per_gas: Vec<String>
+}
