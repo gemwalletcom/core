c2439ca3058e4f2a2f85d69a8e476b067ebec0e3
timdegen <104884878+timdegen@users.noreply.github.com>
2023-09-07 21:24:50 +0100

    Improve device init migration

--- storage/src/migrations/2023-09-03-192926_devices/up.sql
+++ storage/src/migrations/2023-09-03-192926_devices/up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE devices (
     id SERIAL PRIMARY KEY,
-    device_id VARCHAR(128) NOT NULL,
+    device_id VARCHAR(32) NOT NULL,
     is_push_enabled boolean NOT NULL,
     platform VARCHAR(8) NOT NULL,
-    token VARCHAR(128) NOT NULL,
+    token VARCHAR(256) NOT NULL,
     locale VARCHAR(8) NOT NULL,
     updated_at timestamp NOT NULL default current_timestamp,
     created_at timestamp NOT NULL default current_timestamp,
