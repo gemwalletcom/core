fd4cc29ec45a869b68f508675f0862a3db60837f
timdegen <104884878+timdegen@users.noreply.github.com>
2023-09-26 20:58:06 +0100

    Add TronTransactionReceipt

--- blockchain/src/tron/models/tron_transaction.rs
+++ blockchain/src/tron/models/tron_transaction.rs
@@ -13,3 +13,17 @@ struct TronTransaction {
 struct TronTransactionContractRef {
     contractRet: String,
 }
+
+#[typeshare]
+struct TronTransactionReceipt {
+    #[serde(rename = "blockNumber")]
+    pub block_number: i32, 
+    fee: Option<i32>,
+    result: Option<String>,
+    receipt: Option<TronReceipt>,
+}
+
+#[typeshare]
+struct TronReceipt {
+    result: Option<String>,
+}
