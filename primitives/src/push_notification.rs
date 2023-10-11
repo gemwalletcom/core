fb649a640cfbaab1fe905ee11ebd771d0ea075f8
timdegen <104884878+timdegen@users.noreply.github.com>
2023-10-11 22:57:54 +0100

    Add typeshare for PushNotificationTypes

--- primitives/src/push_notification.rs
+++ primitives/src/push_notification.rs
@@ -2,14 +2,15 @@ use typeshare::typeshare;
 use serde::{Serialize, Deserialize};
 use crate::Transaction;
 
+#[typeshare(swift = "Equatable, Codable")]
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum PushNotificationTypes {
     Transaction,
 }
 
+#[typeshare(swift = "Equatable, Codable")]
 #[derive(Debug, Serialize, Deserialize)]
-#[typeshare(swift = "Equatable, Codable, Hashable")]
 pub struct PushNotification {
     #[serde(rename = "type")]
     pub notification_type: PushNotificationTypes,
