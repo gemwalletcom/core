7c6fe08006a19c49d2d2d94ee1a9afed0b7ec434
timdegen <104884878+timdegen@users.noreply.github.com>
2023-10-15 01:15:41 +0100

    Add is buy and swap enabled to AssetMetaData

--- primitives/src/asset_metadata.rs
+++ primitives/src/asset_metadata.rs
@@ -2,4 +2,8 @@
 struct AssetMetaData {
     #[serde(rename = "isEnabled")]
     is_enabled: bool,
+    #[serde(rename = "isBuyEnabled")]
+    is_buy_enabled: bool,
+    #[serde(rename = "isSwapEnabled")]
+    is_swap_enabled: bool,
 }
