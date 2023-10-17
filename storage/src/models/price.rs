fd4f84db4a92dd55e1fb65c31eddbe73f2964790
timdegen <104884878+timdegen@users.noreply.github.com>
2023-10-17 03:55:23 +0100

    Update price.rs

--- storage/src/models/price.rs
+++ storage/src/models/price.rs
@@ -87,7 +87,7 @@ impl Price {
 
     pub fn as_market_primitive(&self) -> AssetMarket {
         AssetMarket{
-            market_cap: self.market_cap.into(),
+            market_cap: Some(self.market_cap).into(),
             market_cap_rank: Some(self.market_cap_rank),
             total_volume: self.total_volume.into(),
         }
