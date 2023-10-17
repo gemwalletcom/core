4de8dea0e247f0b9c552c89b9b28c4f898468a07
timdegen <104884878+timdegen@users.noreply.github.com>
2023-10-17 01:09:51 +0100

    Fix move issue

--- api/src/asset_client.rs
+++ api/src/asset_client.rs
@@ -20,9 +20,9 @@ impl AssetsClient {
 
     pub fn get_asset_full(&mut self, asset_id: &str) -> Result<AssetFull, Box<dyn Error>> {
         let asset = self.database.get_asset(asset_id.to_string())?.as_primitive();
-        let asset_price = self.database.get_price(asset_id);
-        let market = asset_price.ok().map(|x| x.as_market_primitive());
-        let price = asset_price.ok().map(|x| x.as_price_primitive());
+        let asset_price = self.database.get_price(asset_id).ok();
+        let market = asset_price.clone().map(|x| x.as_market_primitive());
+        let price = asset_price.clone().clone().map(|x| x.as_price_primitive());
         let details = self.database.get_asset_details(asset_id.to_string()).ok().map(|x| x.as_primitive());
         Ok(AssetFull{asset, details, price, market})
     }
