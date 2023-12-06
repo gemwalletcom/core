c85c8a19c372a8cead1c2df0822847af04cc1cc9
timdegen <104884878+timdegen@users.noreply.github.com>
2023-12-06 04:26:27 +0000

    Update price_updater.rs

--- pricer/src/price_updater.rs
+++ pricer/src/price_updater.rs
@@ -96,22 +96,39 @@ impl PriceUpdater {
     pub async fn update_charts(&mut self) -> Result<usize, Box<dyn std::error::Error>> {
         let coin_list = self.coin_gecko_client.get_coin_list().await?;
 
+
+        let mut start = false;
+
         for coin_id in coin_list.clone() {
-            let prices = self.coin_gecko_client.get_market_chart(coin_id.id.as_str()).await?;
-
-            println!("update_charts: {}, count: {:?}", coin_id.id.as_str(), prices.prices.len());
-
-            if prices.prices.len() < 10 {
+            
+            if coin_id.id == "phonon-dao" {
+                start = true;
                 continue;
             }
 
-            let charts = prices.prices.into_iter().map(|x| 
-                ChartCoinPrice{ coin_id: coin_id.id.clone(), price: x[1], created_at: x[0] as u64 } 
-            ).collect::<Vec<ChartCoinPrice>>();
+            if start == false {
+                continue;
+            }
 
-            let _ = self.price_client.set_charts(charts).await?;
+            let prices = self.coin_gecko_client.get_market_chart(coin_id.id.as_str()).await;
 
-            thread::sleep(Duration::from_millis(100));
+            match prices {
+                Ok(prices) => {
+                    let charts = prices.prices.clone().into_iter().map(|x| 
+                        ChartCoinPrice{ coin_id: coin_id.id.clone(), price: x[1], created_at: x[0] as u64 } 
+                    ).collect::<Vec<ChartCoinPrice>>();
+        
+                    let _ = self.price_client.set_charts(charts).await?;
+        
+                    println!("update_charts: {}, count: {:?}", coin_id.id.as_str(), prices.prices.len());
+
+                    thread::sleep(Duration::from_millis(100));
+                }
+                Err(err) => {
+                    println!("update charts: {}, error: {:?}", coin_id.id.as_str(), err);
+                    continue;
+                } 
+            }
         }
         Ok(coin_list.len())
     }
