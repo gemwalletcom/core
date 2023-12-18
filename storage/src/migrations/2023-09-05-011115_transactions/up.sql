911db494600729e7bfc1eb81c64cbbd3e4309dba
timdegen <104884878+timdegen@users.noreply.github.com>
2023-12-18 21:45:48 +0000

    Add chain reference table to transactions

--- storage/src/migrations/2023-09-05-011115_transactions/up.sql
+++ storage/src/migrations/2023-09-05-011115_transactions/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE transactions (
     id VARCHAR(256) NOT NULL PRIMARY KEY,
-    chain VARCHAR(16) NOT NULL,
+    chain VARCHAR(16) NOT NULL REFERENCES chains (id) ON DELETE CASCADE,
     hash VARCHAR(256) NOT NULL,
     from_address VARCHAR(256),
     to_address VARCHAR(256),
