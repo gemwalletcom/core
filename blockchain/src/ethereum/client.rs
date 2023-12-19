e24f72bfefddac7d3859e7864b754d42c36bd9f2
timdegen <104884878+timdegen@users.noreply.github.com>
2023-12-19 03:55:56 +0000

    Increase response size for ethereum client

--- blockchain/src/ethereum/client.rs
+++ blockchain/src/ethereum/client.rs
@@ -32,7 +32,10 @@ pub struct EthereumClient {
 
 impl EthereumClient {
     pub fn new(chain: Chain, url: String) -> Self {
-        let client = HttpClientBuilder::default().build(url).unwrap();
+        let client = HttpClientBuilder::default()
+            .max_response_size(100 * 1024 * 1024) // 100MB
+            .build(url)
+            .unwrap();
 
         Self { chain, client }
     }
