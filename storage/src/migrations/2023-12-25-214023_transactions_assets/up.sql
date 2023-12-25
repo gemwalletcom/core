0c04439a8311f54434430ef2c3ffba7426ef1171
timdegen <104884878+timdegen@users.noreply.github.com>
2023-12-25 23:15:25 +0000

    Add chain_id to transactions_assets

--- storage/src/migrations/2023-12-25-214023_transactions_assets/up.sql
+++ storage/src/migrations/2023-12-25-214023_transactions_assets/up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE transactions_assets (
     id SERIAL PRIMARY KEY,
+    chain_id VARCHAR(32) NOT NULL REFERENCES chains (id) ON DELETE CASCADE,
     transaction_id VARCHAR(256) NOT NULL REFERENCES transactions (id) ON DELETE CASCADE ,
     asset_id VARCHAR(256) NOT NULL REFERENCES assets (id) ON DELETE CASCADE,
     UNIQUE(transaction_id, asset_id)
