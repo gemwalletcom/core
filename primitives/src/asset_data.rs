120fd41afc1f7273ec7bb0a1c4f0e40a942c51d0
timdegen <104884878+timdegen@users.noreply.github.com>
2024-01-11 17:35:40 +0000

    Add account to AssetData

--- primitives/src/asset_data.rs
+++ primitives/src/asset_data.rs
@@ -2,7 +2,8 @@
 struct AssetData {
     asset: Asset,
     balance: Balance,
+    account: Account,
     price: Option<Price>,
     details: Option<AssetDetailsInfo>,
-    metadata: AssetMetaData
+    metadata: AssetMetaData,
 }
