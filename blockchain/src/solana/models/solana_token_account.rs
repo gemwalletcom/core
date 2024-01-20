d0a12ea4b225159f868635ce70ce5ce69a5e1b0f
timdegen <104884878+timdegen@users.noreply.github.com>
2024-01-20 00:19:52 +0000

    Add SolanaStakeAccountDataParsedInfoMeta

--- blockchain/src/solana/models/solana_token_account.rs
+++ blockchain/src/solana/models/solana_token_account.rs
@@ -29,6 +29,13 @@ struct SolanaStakeAccountDataParsed {
 #[typeshare]
 struct SolanaStakeAccountDataParsedInfo {
     stake: SolanaStakeAccountDataParsedInfoStake,
+    meta: SolanaStakeAccountDataParsedInfoMeta,
+}
+
+#[typeshare]
+#[serde(rename_all = "camelCase")]
+struct SolanaStakeAccountDataParsedInfoMeta {
+    rent_exempt_reserve: String,
 }
 
 #[typeshare]
