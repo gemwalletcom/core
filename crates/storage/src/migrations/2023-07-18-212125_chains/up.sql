1d811756dbb64be919ee0403beb230a325dc770d
timdegen <104884878+timdegen@users.noreply.github.com>
2024-02-24 04:02:46 +0000

    Improve chains table creation

--- crates/storage/src/migrations/2023-07-18-212125_chains/up.sql
+++ crates/storage/src/migrations/2023-07-18-212125_chains/up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS chains (
+CREATE TABLE chains (
     id VARCHAR(32) PRIMARY KEY NOT NULL,
 
     updated_at timestamp NOT NULL default current_timestamp,
@@ -6,14 +6,3 @@ CREATE TABLE IF NOT EXISTS chains (
 );
 
 SELECT diesel_manage_updated_at('chains');
-
-INSERT INTO "chains" ("id") VALUES ('ethereum');
-INSERT INTO "chains" ("id") VALUES ('cosmos');
-INSERT INTO "chains" ("id") VALUES ('tron');
-INSERT INTO "chains" ("id") VALUES ('smartchain');
-INSERT INTO "chains" ("id") VALUES ('binance');
-INSERT INTO "chains" ("id") VALUES ('osmosis');
-INSERT INTO "chains" ("id") VALUES ('solana');
-INSERT INTO "chains" ("id") VALUES ('arbitrum');
-INSERT INTO "chains" ("id") VALUES ('polygon');
-INSERT INTO "chains" ("id") VALUES ('bitcoin');
