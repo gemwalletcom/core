136133f91cfcdec55b79ee2d926466dd8652e157
timdegen <104884878+timdegen@users.noreply.github.com>
2024-03-07 00:31:28 +0000

    Update mod.rs

--- gemstone/src/explorer/mod.rs
+++ gemstone/src/explorer/mod.rs
@@ -17,6 +17,7 @@ pub fn get_name_by_host(host: String) -> Option<String> {
         "blockchair.com" => Some("Blockchair".into()),
         "tronscan.org" => Some("TRONSCAN".into()),
         "basescan.org" => Some("BaseScan".into()),
+        "blastscan.io" => Some("BlastScan".into()),
         "explorer.sui.io" | "suiexplorer.com" => Some("Sui Explorer".into()),
         "explorer.aptoslabs.com" => Some("Aptos Explorer".into()),
         "mintscan.io" | "www.mintscan.io" => Some("MintScan".into()),
