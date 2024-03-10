79afcfdedfde1fd791a1555e6260c6fd9654abec
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-03-10 21:34:32 +0900

    replace rust_decimal with BigDecimal

--- crates/primitives/src/numer_formatter.rs
+++ crates/primitives/src/numer_formatter.rs
@@ -1,12 +1,16 @@
-use rust_decimal::{prelude::*, Decimal};
+use bigdecimal::BigDecimal;
+use num_bigint::BigInt;
+use num_traits::FromPrimitive;
+use std::str::FromStr;
+
 pub struct NumberFormatter {}
 
 impl NumberFormatter {
     pub fn value(value: &str, decimals: i32) -> Option<String> {
-        let mut crypto_amount: Decimal = Decimal::from_str(value).ok()?;
-        crypto_amount.set_scale(decimals as u32).ok()?;
-        let amount: f64 = crypto_amount.to_f64()?;
-        format!("{}", amount).into()
+        let mut decimal = BigDecimal::from_str(value).ok()?;
+        let exp = BigInt::from_i32(10)?.pow(decimals as u32);
+        decimal = decimal / BigDecimal::from(exp);
+        Some(decimal.to_string())
     }
 }
 
@@ -17,18 +21,29 @@ mod tests {
     #[test]
     fn test_value() {
         // Test case 1: Valid input
-        let result = NumberFormatter::value("123456", 3);
-        assert_eq!(result, Some("123.456".to_string()));
+        let result = NumberFormatter::value("123456", 3).unwrap();
+        assert_eq!(result, "123.456");
 
         // Test case 2: Input with more decimals than specified
-        let result = NumberFormatter::value("789123456", 4);
-        assert_eq!(result, Some("78912.3456".to_string()));
+        let result = NumberFormatter::value("789123456", 4).unwrap();
+        assert_eq!(result, "78912.3456");
 
         // Test case 3: Input with fewer decimals than specified
-        let result = NumberFormatter::value("4567", 4);
-        assert_eq!(result, Some("0.4567".to_string()));
+        let result = NumberFormatter::value("4567", 4).unwrap();
+        assert_eq!(result, "0.4567");
 
-        // Test case 4: Invalid input
+        // Test case 4: u256 input
+        let result = NumberFormatter::value(
+            "115792089237316195423570985008687907853269984665640564039457000000000000000000",
+            18,
+        )
+        .unwrap();
+        assert_eq!(
+            result,
+            "115792089237316195423570985008687907853269984665640564039457"
+        );
+
+        // Test case 5: Invalid input
         let result = NumberFormatter::value("abc", 2);
         assert_eq!(result, None);
     }
