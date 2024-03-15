d99346ca65be9a5bdc64748d7f91446060262d95
Kay <kehiiiiya@gmail.com>
2024-03-15 09:28:37 +0000

    fix: showing fee in atmoic unit on HTTP transaction page

--- gemstone/src/cosmos/mod.rs
+++ gemstone/src/cosmos/mod.rs
@@ -1,18 +1,18 @@
 use bech32::{convert_bits, FromBase32, ToBase32};
 
-fn convert_cosmos_address(address: &str, hrp: &str) -> String {
-    let (hrp_cosmos, data_cosmos) = bech32::decode(address).expect("Invalid cosmos address");
+fn convert_cosmos_address(address: &str, hrp: &str) -> Result<String, Error> {
+    let (hrp_cosmos, data_cosmos) = bech32::decode(address)?;
     let data_bytes =
-        bech32::FromBase32::from_base32(&data_cosmos).expect("Failed to convert data to bytes");
+        bech32::FromBase32::from_base32(&data_cosmos)?;
 
-    let mut data_bits = convert_bits(&data_bytes, 5, 8, false).expect("Failed to convert bits");
+    let mut data_bits = convert_bits(&data_bytes, 5, 8, false)?;
     data_bits.insert(0, 0);
 
     let data_chain_bytes =
-        convert_bits(&data_bits, 8, 5, false).expect("Failed to convert bits back to bytes");
+        convert_bits(&data_bits, 8, 5, false)?;
     let data_chain = bech32::ToBase32::to_base32(&data_chain_bytes);
 
-    bech32::encode(hrp, data_chain).expect("Failed to encode chain address")
+    bech32::encode(hrp, data_chain)?
 }
 
 #[cfg(test)]
@@ -20,7 +20,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_cosmos_address_convert() {
+    fn test_cosmos_address_convert_expected_case() {
         let cosmos_address = "cosmos1h3laqcrmul79zwtw6j63ncsl0adfj07wgupylj";
         let osmosis_hrp = "osmosis";
 
@@ -28,5 +28,16 @@ mod tests {
 
         let output_address = convert_cosmos_address(cosmos_address, osmosis_hrp);
         assert_eq(expected, output_address);
+    }
+
+    #[test]
+    fn test_cosmos_address_convert_unexpected_case() {
+        let cosmos_address = "cosmos1h3laqcrmul79zwtw6j63ncsl0adfj07wgupylj";
+        let osmosis_hrp = "gemstone";
+
+        let expected = "osmosis1h3laqcrmul79zwtw6j63ncsl0adfj07wm8vf00";
+
+        let output_address = convert_cosmos_address(cosmos_address, osmosis_hrp);
+        assert_nq(expected, output_address);
     }
 }
