d40cd81a3aa1282c4f7dc9ca800717ddc0e0c46a
timdegen <104884878+gemcoder21@users.noreply.github.com>
2024-03-25 03:41:41 +0000

    Add get_chain_for_coingecko_platform_id

--- crates/coingecko/src/lib.rs
+++ crates/coingecko/src/lib.rs
@@ -3,5 +3,7 @@ pub mod mapper;
 pub mod model;
 
 pub use self::client::CoinGeckoClient;
-pub use self::mapper::{get_associated_chains, get_chain_for_coingecko_id};
+pub use self::mapper::{
+    get_associated_chains, get_chain_for_coingecko_id, get_chain_for_coingecko_platform_id,
+};
 pub use self::model::{Coin, CoinInfo, CoinMarket};
--- crates/coingecko/src/mapper.rs
+++ crates/coingecko/src/mapper.rs
@@ -1,16 +1,37 @@
 use primitives::chain::Chain;
 
+pub fn get_chain_for_coingecko_platform_id(id: &str) -> Option<Chain> {
+    match id {
+        "ethereum" => Some(Chain::Ethereum),
+        "avalanche" => Some(Chain::AvalancheC),
+        "optimistic-ethereum" => Some(Chain::Optimism),
+        "base" => Some(Chain::Base),
+        "arbitrum-one" => Some(Chain::Arbitrum),
+        "binance-smart-chain" => Some(Chain::SmartChain),
+        "manta-pacific" => Some(Chain::Manta),
+        "tron" => Some(Chain::Tron),
+        "polygon-pos" => Some(Chain::Polygon),
+        "solana" => Some(Chain::Solana),
+        "blast" => Some(Chain::Blast),
+        "xdai" => Some(Chain::Gnosis),
+        "fantom" => Some(Chain::Fantom),
+        "osmosis" => Some(Chain::Osmosis),
+        "cosmos" => Some(Chain::Cosmos),
+        "aptos" => Some(Chain::Aptos),
+        "suiche" => Some(Chain::Sui),
+        "opbnb" => Some(Chain::OpBNB),
+        _ => None,
+    }
+}
+
 pub fn get_chain_for_coingecko_id(id: &str) -> Option<Chain> {
     match id {
         "bitcoin" => Some(Chain::Bitcoin),
         "litecoin" => Some(Chain::Litecoin),
         "binancecoin" => Some(Chain::Binance),
         "ethereum" => Some(Chain::Ethereum),
-        "binance-smart-chain" => Some(Chain::SmartChain),
-        "matic-network" | "polygon-pos" => Some(Chain::Polygon),
+        "matic-network" => Some(Chain::Polygon),
         "solana" => Some(Chain::Solana),
-        "arbitrum-one" => Some(Chain::Arbitrum),
-        "optimistic-ethereum" => Some(Chain::Optimism),
         "thorchain" => Some(Chain::Thorchain),
         "cosmos" => Some(Chain::Cosmos),
         "osmosis" => Some(Chain::Osmosis),
@@ -18,7 +39,7 @@ pub fn get_chain_for_coingecko_id(id: &str) -> Option<Chain> {
         "tron" => Some(Chain::Tron),
         "dogecoin" => Some(Chain::Doge),
         "aptos" => Some(Chain::Aptos),
-        "avalanche" | "avalanche-2" => Some(Chain::AvalancheC),
+        "avalanche-2" => Some(Chain::AvalancheC),
         "sui" => Some(Chain::Sui),
         "ripple" => Some(Chain::Xrp),
         "gnosis" => Some(Chain::Gnosis),
@@ -26,7 +47,7 @@ pub fn get_chain_for_coingecko_id(id: &str) -> Option<Chain> {
         "celestia" => Some(Chain::Celestia),
         "injective" | "injective-protocol" => Some(Chain::Injective),
         "sei-network" => Some(Chain::Sei),
-        "manta-network" | "manta-pacific" => Some(Chain::Manta),
+        "manta-network" => Some(Chain::Manta),
         _ => None,
     }
 }
