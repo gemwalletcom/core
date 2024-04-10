168b85513645c55070840702b31f3b69b31c58b1
timdegen <104884878+gemcoder21@users.noreply.github.com>
2024-04-10 18:36:19 +0100

    Complete near blockchain transfers

--- crates/gem_chain_rpc/src/near/client.rs
+++ crates/gem_chain_rpc/src/near/client.rs
@@ -2,13 +2,14 @@ use std::error::Error;
 
 use crate::ChainProvider;
 use async_trait::async_trait;
+use chrono::Utc;
 use jsonrpsee::{
     core::{client::ClientT, params::ObjectParams},
     http_client::{HttpClient, HttpClientBuilder},
 };
-use primitives::{chain::Chain, Transaction};
+use primitives::{chain::Chain, Transaction, TransactionState, TransactionType};
 
-use super::model::Block;
+use super::model::{Block, BlockHeader, Chunk, TransactionDeposit, TRANSFER_ACTION};
 
 pub struct NearClient {
     client: HttpClient,
@@ -23,6 +24,65 @@ impl NearClient {
 
         Self { client }
     }
+
+    async fn get_final_block(&self) -> Result<Block, Box<dyn Error + Send + Sync>> {
+        let mut params = ObjectParams::new();
+        params.insert("finality", "final").unwrap();
+        let block: Block = self.client.request("block", params).await?;
+        Ok(block)
+    }
+
+    async fn get_block(&self, block: i64) -> Result<Block, Box<dyn Error + Send + Sync>> {
+        let mut params = ObjectParams::new();
+        params.insert("block_id", block).unwrap();
+        let block: Block = self.client.request("block", params).await?;
+        Ok(block)
+    }
+
+    async fn get_chunk(
+        &self,
+        block: i64,
+        shard_id: i64,
+    ) -> Result<Chunk, Box<dyn Error + Send + Sync>> {
+        let mut params = ObjectParams::new();
+        params.insert("block_id", block).unwrap();
+        params.insert("shard_id", shard_id).unwrap();
+        let chunk: Chunk = self.client.request("chunk", params).await?;
+        Ok(chunk)
+    }
+
+    fn map_transaction(
+        &self,
+        header: BlockHeader,
+        transaction: super::model::Transaction,
+    ) -> Option<primitives::Transaction> {
+        if transaction.actions.len() == 1 {
+            let asset_id = self.get_chain().as_asset_id();
+            if let Some(value) = transaction.actions[0].get(TRANSFER_ACTION) {
+                let deposit: TransactionDeposit = serde_json::from_value(value.clone()).ok()?;
+
+                let transaction = primitives::Transaction::new(
+                    transaction.hash,
+                    asset_id.clone(),
+                    transaction.signer_id,
+                    transaction.receiver_id,
+                    None,
+                    TransactionType::Transfer,
+                    TransactionState::Confirmed,
+                    header.height.to_string(),
+                    transaction.nonce.to_string(),
+                    "830000000000000000000".to_string(),
+                    asset_id,
+                    deposit.deposit,
+                    None,
+                    None,
+                    Utc::now(),
+                );
+                return Some(transaction);
+            }
+        }
+        None
+    }
 }
 
 #[async_trait]
@@ -32,18 +92,32 @@ impl ChainProvider for NearClient {
     }
 
     async fn get_latest_block(&self) -> Result<i64, Box<dyn Error + Send + Sync>> {
-        let mut params = ObjectParams::new();
-        params.insert("finality", "final").unwrap();
-
-        let block: Block = self.client.request("block", params).await?;
+        let block = self.get_final_block().await?;
         Ok(block.header.height)
     }
 
     async fn get_transactions(
         &self,
-        _block_number: i64,
+        block_number: i64,
     ) -> Result<Vec<Transaction>, Box<dyn Error + Send + Sync>> {
-        //TODO: Implement fetching transactions
-        Ok(vec![])
+        let block = self.get_block(block_number).await?;
+        let chunks = futures::future::try_join_all(
+            block
+                .chunks
+                .into_iter()
+                .map(|chunk| self.get_chunk(block.header.height, chunk.shard_id)),
+        )
+        .await?;
+
+        let transactions = chunks
+            .into_iter()
+            .flat_map(|x| {
+                x.transactions
+                    .into_iter()
+                    .flat_map(|x| self.map_transaction(block.header.clone(), x))
+            })
+            .collect();
+
+        Ok(transactions)
     }
 }
--- crates/gem_chain_rpc/src/near/model.rs
+++ crates/gem_chain_rpc/src/near/model.rs
@@ -1,12 +1,42 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+pub const TRANSFER_ACTION: &str = "Transfer";
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Block {
     pub header: BlockHeader,
+    pub chunks: Vec<ChunkHeader>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase")]
 pub struct BlockHeader {
     pub height: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ChunkHeader {
+    pub shard_id: i64,
+    pub gas_used: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Chunk {
+    pub transactions: Vec<Transaction>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Transaction {
+    pub hash: String,
+    pub signer_id: String,
+    pub receiver_id: String,
+    pub nonce: i64,
+    pub actions: Vec<HashMap<String, Value>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TransactionDeposit {
+    pub deposit: String,
 }
