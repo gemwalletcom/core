329f1190496ab7937fdba0abca806883fb531649
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-04-28 10:00:57 +0900

    return error instead of unwrap

--- apps/api/src/response.rs
+++ apps/api/src/response.rs
@@ -4,3 +4,16 @@ use serde::{Deserialize, Serialize};
 pub struct ResponseResults<T> {
     pub results: Vec<T>,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseError {
+    pub error: String,
+}
+
+impl From<Box<dyn std::error::Error + Send + Sync>> for ResponseError {
+    fn from(error: Box<dyn std::error::Error + Send + Sync>) -> Self {
+        Self {
+            error: error.to_string(),
+        }
+    }
+}
