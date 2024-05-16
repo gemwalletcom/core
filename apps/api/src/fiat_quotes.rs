f008279fa912223ee5837044b0acfdca1aca2ea4
timdegen <104884878+gemcoder21@users.noreply.github.com>
2024-05-17 00:18:06 +0100

    Add fiat on ramp routes

--- apps/api/src/fiat_quotes.rs
+++ apps/api/src/fiat_quotes.rs
@@ -8,6 +8,44 @@ use rocket::serde::json::Json;
 use rocket::tokio::sync::Mutex;
 use rocket::State;
 
+// on ramp
+
+#[get("/fiat/on_ramp/quotes/<asset_id>?<amount>&<currency>&<wallet_address>&<ip_address>")]
+pub async fn get_fiat_on_ramp_quotes(
+    asset_id: String,
+    amount: f64,
+    currency: String,
+    wallet_address: String,
+    ip_address: Option<String>,
+    ip: std::net::IpAddr,
+    fiat_client: &State<Mutex<FiatProvider>>,
+) -> Json<FiatQuotes> {
+    get_fiat_quotes(
+        asset_id,
+        amount,
+        currency,
+        wallet_address,
+        ip_address,
+        ip,
+        fiat_client,
+    )
+    .await
+}
+
+#[get("/fiat/on_ramp/assets")]
+pub async fn get_fiat_on_ramp_assets(fiat_client: &State<Mutex<FiatProvider>>) -> Json<FiatAssets> {
+    get_fiat_assets(fiat_client).await
+}
+
+#[get("/fiat/on_ramp/rates")]
+pub async fn get_fiat_on_ramp_rates(fiat_client: &State<Mutex<FiatProvider>>) -> Json<FiatRates> {
+    get_fiat_rates(fiat_client).await
+}
+
+// off ramp
+
+// old routes. switched to use on_ramp and off_ramp routes
+
 #[get("/fiat/quotes/<asset_id>?<amount>&<currency>&<wallet_address>&<ip_address>")]
 pub async fn get_fiat_quotes(
     asset_id: String,
