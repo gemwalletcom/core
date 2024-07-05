f0afceb7dd87b294c663c6ef20fbf7d358823477
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-07-05 16:57:44 +0900

    add runescan for thorchain

--- crates/primitives/src/explorers/thorchain.rs
+++ crates/primitives/src/explorers/thorchain.rs
@@ -29,3 +29,32 @@ impl BlockExplorer for Viewblock {
         None
     }
 }
+
+pub struct RuneScan {
+    pub meta: Metadata,
+}
+
+impl RuneScan {
+    pub fn new() -> Box<Self> {
+        Box::new(Self {
+            meta: Metadata {
+                name: "RuneScan",
+                base_url: "https://runescan.io",
+            },
+        })
+    }
+}
+impl BlockExplorer for RuneScan {
+    fn name(&self) -> String {
+        self.meta.name.into()
+    }
+    fn get_tx_url(&self, hash: &str) -> String {
+        format!("{}/tx/{}", self.meta.base_url, hash)
+    }
+    fn get_address_url(&self, address: &str) -> String {
+        format!("{}/address/{}", self.meta.base_url, address)
+    }
+    fn get_token_url(&self, _token: &str) -> Option<String> {
+        None
+    }
+}
