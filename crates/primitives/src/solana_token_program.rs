833928c54447324ffe7a3bb231704685640f2fe4
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-08-24 09:11:36 +0900

    Update solana_token_program.rs

--- crates/primitives/src/solana_token_program.rs
+++ crates/primitives/src/solana_token_program.rs
@@ -3,7 +3,7 @@ use strum::{AsRefStr, EnumString};
 use typeshare::typeshare;
 
 #[derive(Debug, Serialize, AsRefStr, EnumString)]
-#[typeshare(swift = "Equatable, Codable, Hashable")]
+#[typeshare(swift = "Equatable, Codable, CaseIterable")]
 #[serde(rename_all = "lowercase")]
 #[strum(serialize_all = "lowercase")]
 pub enum SolanaTokenProgramId {
