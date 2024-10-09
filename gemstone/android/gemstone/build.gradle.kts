272b7dc48e8bb3450bac58bfa2fdd75196671aa5
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-10-09 19:58:20 +0900

    link to module

--- gemstone/android/gemstone/build.gradle.kts
+++ gemstone/android/gemstone/build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
     api("net.java.dev.jna:jna:5.15.0@aar"){ artifact { type = "aar" } }
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
