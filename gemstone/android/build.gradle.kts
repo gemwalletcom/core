c600bc9b846428601aedb377f6a1b00dfcde942d
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-10-10 14:43:09 +0900

    bump gradle version

--- gemstone/android/build.gradle.kts
+++ gemstone/android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
+    id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.github.willir.rust.cargo-ndk-android") version "0.3.4" apply false
 }
