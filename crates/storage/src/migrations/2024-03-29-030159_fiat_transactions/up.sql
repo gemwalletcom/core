536a84ad906c929be580d7434d16734bad002c98
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2024-11-17 01:35:58 +0700

    Support building Gemstone on x86_64 arch (#303)

--- crates/storage/src/migrations/2024-03-29-030159_fiat_transactions/up.sql
+++ crates/storage/src/migrations/2024-03-29-030159_fiat_transactions/up.sql
@@ -1,7 +1,5 @@
 CREATE TABLE fiat_transactions (
-    id SERIAL PRIMARY KEY,
-
-    transaction_type VARCHAR(128) NOT NULL default 'buy',
+    id SERIAL PRIMARY KEY,    
 
     provider_id VARCHAR(128) NOT NULL REFERENCES fiat_providers (id) ON DELETE CASCADE,
     asset_id VARCHAR(128) REFERENCES assets (id) ON DELETE CASCADE,
@@ -20,6 +18,8 @@ CREATE TABLE fiat_transactions (
 
     updated_at timestamp NOT NULL default current_timestamp,
     created_at timestamp NOT NULL default current_timestamp,
+
+    transaction_type VARCHAR(32) NOT NULL default 'buy',
 
     UNIQUE NULLS NOT DISTINCT(provider_id, provider_transaction_id)
 );
