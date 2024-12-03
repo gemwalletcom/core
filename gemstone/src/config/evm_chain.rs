b1d5008f67222409c8afe2f7ce3e82cfa272b2b1
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2024-12-03 21:58:49 +0000

    Add EVMHistoryRewardPercentiles

--- gemstone/src/config/evm_chain.rs
+++ gemstone/src/config/evm_chain.rs
@@ -4,11 +4,24 @@ use primitives::EVMChain;
 pub struct EVMChainConfig {
     pub min_priority_fee: u64,
     pub is_opstack: bool,
+    pub rewards_percentiles: EVMHistoryRewardPercentiles,
+}
+
+#[derive(uniffi::Record, Debug, Clone, PartialEq)]
+pub struct EVMHistoryRewardPercentiles {
+    pub slow: i64,
+    pub normal: i64,
+    pub fast: i64,
 }
 
 pub fn get_evm_chain_config(chain: EVMChain) -> EVMChainConfig {
     EVMChainConfig {
         min_priority_fee: chain.min_priority_fee(),
         is_opstack: chain.is_opstack(),
+        rewards_percentiles: EVMHistoryRewardPercentiles {
+            slow: 25,
+            normal: 50,
+            fast: 75,
+        },
     }
 }
