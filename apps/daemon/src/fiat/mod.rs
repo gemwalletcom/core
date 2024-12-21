79d616a215a171ac3aa6c3846682e30f3891d953
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2024-12-21 10:14:01 +0000

    Add daemon fiat jobs

---
+++ apps/daemon/src/fiat/mod.rs
@@ -0,0 +1,22 @@
+use fiat::FiatProviderFactory;
+use fiat_assets_updater::FiatAssetsUpdater;
+use job_runner::run_job;
+use settings::Settings;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::time::Duration;
+mod fiat_assets_updater;
+
+pub async fn jobs(settings: Settings) -> Vec<Pin<Box<dyn Future<Output = ()> + Send>>> {
+    let update_fiat_assets_job = run_job("Price Alerts", Duration::from_secs(settings.alerter.update_interval_seconds), {
+        let settings = Arc::new(settings.clone());
+        move || {
+            let providers = FiatProviderFactory::new_providers((*settings).clone());
+            let mut fiat_assets_updater = FiatAssetsUpdater::new(&settings.postgres.url, providers);
+            async move { fiat_assets_updater.update_fiat_assets().await }
+        }
+    });
+
+    vec![Box::pin(update_fiat_assets_job)]
+}
