377c4626014cef31ef3d322d11ea71a28cab7a96
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-01-08 10:56:07 +0900

    Update chain_cosmos.rs (#363)

--- crates/primitives/src/chain_cosmos.rs
+++ crates/primitives/src/chain_cosmos.rs
@@ -3,9 +3,10 @@ use strum::EnumIter;
 use strum_macros::{AsRefStr, EnumString};
 use typeshare::typeshare;
 
-#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
+#[derive(Copy, Clone, Debug, Serialize, Deserialize, EnumString)]
 #[typeshare(swift = "Equatable, CaseIterable, Sendable")]
 #[serde(rename_all = "lowercase")]
+#[strum(serialize_all = "lowercase")]
 pub enum CosmosChain {
     Cosmos,
     Osmosis,
