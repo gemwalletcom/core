27681e3b7d44306fea9bb1666c3c4f722c1577ae
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-02-19 10:18:33 +0900

    Update hyperliquid.rs

--- crates/primitives/src/explorers/hyperliquid.rs
+++ crates/primitives/src/explorers/hyperliquid.rs
@@ -9,7 +9,7 @@ impl HyperLiquid {
         Box::new(Self {
             meta: Metadata {
                 name: "Hyper Explorer",
-                base_url: "https://hyperevm-explorer.vercel.app/",
+                base_url: "https://hyperevm-explorer.vercel.app",
             },
         })
     }
@@ -23,9 +23,30 @@ impl BlockExplorer for HyperLiquid {
         format!("{}/tx/{}", self.meta.base_url, hash)
     }
     fn get_address_url(&self, address: &str) -> String {
-        format!("{}/account/{}", self.meta.base_url, address)
+        format!("{}/address/{}", self.meta.base_url, address)
     }
     fn get_validator_url(&self, validator: &str) -> Option<String> {
         self.get_address_url(validator).into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_tx_url() {
+        let explorer = HyperLiquid::new();
+        let address = "0x0361d1ce0520968024f140cdedaa3c21f793c5a7";
+        let tx_id = "0x971f5a256584fae7248b5eefa5d2e85b5522192aed198a61ae6b542129822659";
+
+        assert_eq!(
+            explorer.get_tx_url(tx_id),
+            "https://hyperevm-explorer.vercel.app/tx/0x971f5a256584fae7248b5eefa5d2e85b5522192aed198a61ae6b542129822659"
+        );
+        assert_eq!(
+            explorer.get_address_url(address),
+            "https://hyperevm-explorer.vercel.app/address/0x0361d1ce0520968024f140cdedaa3c21f793c5a7"
+        );
     }
 }
