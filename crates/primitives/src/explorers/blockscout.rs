081b577827ff4dad4a4f4e402bf819b240c99d96
Ferman <119309671+0xh3rman@users.noreply.github.com>
2025-02-23 08:09:26 +0900

    Add blockscout for hyper evm (#419)

--- crates/primitives/src/explorers/blockscout.rs
+++ crates/primitives/src/explorers/blockscout.rs
@@ -3,12 +3,14 @@ pub struct BlockScout {
     pub meta: Metadata,
 }
 
+static BLOCK_SCOUT: &str = "BlockScout";
+
 impl BlockScout {
     pub fn new_celo() -> Box<Self> {
         Box::new(Self {
             meta: Metadata {
-                name: "BlockScout",
-                base_url: "https://explorer.celo.org/mainnet",
+                name: BLOCK_SCOUT,
+                base_url: "https://celo.blockscout.com",
             },
         })
     }
@@ -27,6 +29,15 @@ impl BlockScout {
             meta: Metadata {
                 name: "Ink Explorer",
                 base_url: "https://explorer.inkonchain.com",
+            },
+        })
+    }
+
+    pub fn new_hyperliquid() -> Box<Self> {
+        Box::new(Self {
+            meta: Metadata {
+                name: BLOCK_SCOUT,
+                base_url: "https://hyperliquid.cloud.blockscout.com",
             },
         })
     }
