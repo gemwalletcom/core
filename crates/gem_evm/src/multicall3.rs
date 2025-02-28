ce0c45274cd2640c315903323c3b21abe995dd08
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-02-28 14:03:33 +0900

    fix hyperliquid multicall3

--- crates/gem_evm/src/multicall3.rs
+++ crates/gem_evm/src/multicall3.rs
@@ -85,8 +85,8 @@ pub fn deployment_by_chain(chain: &EVMChain) -> &'static str {
         | EVMChain::Sonic
         | EVMChain::Berachain
         | EVMChain::Ink
-        | EVMChain::Unichain => "0xcA11bde05977b3631167028862bE2a173976CA11",
+        | EVMChain::Unichain
+        | EVMChain::Hyperliquid => "0xcA11bde05977b3631167028862bE2a173976CA11",
         EVMChain::ZkSync | EVMChain::Abstract => "0xF9cda624FBC7e059355ce98a31693d299FACd963",
-        EVMChain::Hyperliquid => "",
     }
 }
