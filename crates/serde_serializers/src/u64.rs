2b200b7b8b8038f03ccd367de6b979e6ddb40918
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-03-14 00:46:41 +0000

    Add serialize_u64

--- crates/serde_serializers/src/u64.rs
+++ crates/serde_serializers/src/u64.rs
@@ -1,5 +1,12 @@
 use serde::{Deserialize, Deserializer};
 
+pub fn serialize_u64<S>(value: &u64, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&value.to_string())
+}
+
 pub fn deserialize_u64_from_str<'de, D>(deserializer: D) -> Result<u64, D::Error>
 where
     D: Deserializer<'de>,
