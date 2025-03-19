e2ed4787fbb14bee09f8d50ce782a4b827dc1b2c
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-03-19 19:36:26 +0000

    Add pricer_dex package

---
+++ crates/pricer_dex/src/lib.rs
@@ -0,0 +1,20 @@
+use std::error::Error;
+
+use primitives::{AssetId, AssetPrice, Chain};
+
+pub trait PriceChainProvider {
+    fn get_chain_prices(&self, chains: Vec<Chain>) -> Result<Vec<AssetPrice>, Box<dyn Error + Send + Sync>>;
+}
+
+pub trait PriceChainAssetsProvider {
+    fn get_asset_prices(&self, chain: Chain, asset_ids: Vec<AssetId>) -> Result<Vec<AssetPrice>, Box<dyn Error + Send + Sync>>;
+}
+
+pub struct PricesDex {}
+
+impl PricesDex {
+    pub fn get_chain_prices(&self, chains: Vec<Chain>) -> Result<Vec<AssetPrice>, Box<dyn Error + Send + Sync>> {
+        // Implement the logic to get the chain prices here
+        Ok(vec![]) // Placeholder return value
+    }
+}
