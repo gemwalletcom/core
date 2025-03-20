e7c6648210ce6af42ca1b00d2fb29a6c69dd4de6
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-03-20 17:02:17 +0000

    Add pyth integration

---
+++ crates/pricer_dex/src/pyth/mod.rs
@@ -0,0 +1,4 @@
+pub mod client;
+pub mod decoder;
+pub mod model;
+pub mod provider;
---
+++ crates/pricer_dex/tests/integration_test.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    use pricer_dex::{pyth::client::PythClient, PriceChainProvider, PythProvider};
+    use primitives::Chain;
+
+    #[tokio::test]
+    async fn test_get_chain_prices() {
+        let provider = PythProvider {
+            pyth_client: PythClient::new("http://pythnet.rpcpool.com"),
+        };
+        let chains = vec![Chain::Bitcoin, Chain::Ethereum];
+        let result = provider.get_chain_prices(chains.clone()).await;
+
+        println!("{:?}", result);
+
+        assert!(result.is_ok());
+        assert!(result.ok().unwrap().len() == chains.len());
+    }
+}
