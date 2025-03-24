1429a9c804e3920723ec883e5e84b2635f0edafb
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-03-24 18:08:17 +0000

    Fix fiat amount calculation for banxa

--- crates/fiat/src/providers/banxa/mapper.rs
+++ crates/fiat/src/providers/banxa/mapper.rs
@@ -33,6 +33,7 @@ pub fn map_asset_chain(chain: String) -> Option<Chain> {
         "ARB" => Some(Chain::Arbitrum),
         "BASE" => Some(Chain::Base),
         "S" => Some(Chain::Sonic),
+        "INJ" => Some(Chain::Injective),
         _ => None,
     }
 }
