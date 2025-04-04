abf676af6d28f25a51e484ba2a317c5a05667322
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-04-04 21:05:32 +0900

    remove not used method

--- crates/gem_evm/src/address.rs
+++ crates/gem_evm/src/address.rs
@@ -1,10 +1,6 @@
 use alloy_primitives::{Address, AddressError};
 use std::str::FromStr;
 
-pub fn to_checksum(address: &Address) -> String {
-    address.to_checksum(None)
-}
-
 pub fn normalize_checksum(address: &str) -> Result<String, AddressError> {
     let address = Address::from_str(address)?;
     Ok(address.to_checksum(None))
