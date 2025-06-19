2fd1d815100e44f2afab6736cc27efa1cf3ca44e
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-06-19 23:40:46 +0000

    refactor: simplify Ankr transaction model to only store hash and map transaction IDs

--- crates/gem_evm/src/rpc/ankr/mapper.rs
+++ crates/gem_evm/src/rpc/ankr/mapper.rs
@@ -1,63 +1,9 @@
-use crate::{
-    ethereum_address_checksum,
-    rpc::{
-        ankr::Transaction,
-        mapper::{FUNCTION_ERC20_TRANSFER, INPUT_0X},
-    },
-};
-use chrono::DateTime;
-use primitives::Chain;
+use crate::rpc::ankr::Transaction;
 
 pub struct AnkrMapper {}
 
 impl AnkrMapper {
-    pub fn map_transactions(transactions: Vec<Transaction>, chain: Chain) -> Vec<primitives::Transaction> {
-        transactions.into_iter().flat_map(|x| AnkrMapper::map_transaction(x, chain)).collect()
-    }
-
-    pub fn map_transaction(transaction: Transaction, chain: Chain) -> Option<primitives::Transaction> {
-        let fee = transaction.gas_price.clone() * transaction.gas_used.clone();
-        let created_at = DateTime::from_timestamp(transaction.timestamp.to_string().parse::<i64>().ok()?, 0)?;
-        let from = ethereum_address_checksum(&transaction.from).ok()?;
-        let to = ethereum_address_checksum(&transaction.to).ok()?;
-        let status = if transaction.status == "0x1" {
-            primitives::TransactionState::Confirmed
-        } else {
-            primitives::TransactionState::Failed
-        };
-
-        match transaction.input.as_str() {
-            INPUT_0X => Some(primitives::Transaction::new(
-                transaction.hash,
-                chain.as_asset_id(),
-                from,
-                to,
-                None,
-                primitives::TransactionType::Transfer,
-                status,
-                fee.to_string(),
-                chain.as_asset_id(),
-                transaction.value.to_string(),
-                None,
-                None,
-                created_at,
-            )),
-            FUNCTION_ERC20_TRANSFER => None, //TODO: ERC20 Transfer
-            _ => Some(primitives::Transaction::new(
-                transaction.hash,
-                chain.as_asset_id(),
-                from,
-                to.clone(),
-                Some(to.clone()),
-                primitives::TransactionType::SmartContractCall,
-                status,
-                fee.to_string(),
-                chain.as_asset_id(),
-                transaction.value.to_string(),
-                None,
-                None,
-                created_at,
-            )),
-        }
+    pub fn map_transactions_ids(transactions: Vec<Transaction>) -> Vec<String> {
+        transactions.into_iter().map(|x| x.hash).collect()
     }
 }
