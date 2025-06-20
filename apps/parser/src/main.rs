4c6d1c86512b724f9d96c0fe00c1e301bf2923d5
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2025-06-20 03:44:31 +0000

    refactor: split consumer_transactions mode into separate fetch and store modes

--- apps/parser/src/main.rs
+++ apps/parser/src/main.rs
@@ -25,13 +25,12 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     if mode == "consumers" {
         return consumers::run_consumers(settings, database.clone()).await;
-    } else if mode == "consumer_transactions" {
-        tokio::spawn(consumers::run_consumer_store_transactions(settings.clone(), database.clone()));
-        tokio::spawn(consumers::run_consumer_fetch_transactions(settings.clone(), database.clone()));
-        std::future::pending::<()>().await;
-        return Ok(());
-    } else if mode == "consumer_blocks" {
-        return consumers::run_consumer_fetch_blocks(settings, database.clone()).await;
+    } else if mode == "consumer_fetch_transactions" {
+        return consumers::run_consumer_fetch_transactions(settings.clone(), database.clone()).await;
+    } else if mode == "consumer_store_transactions" {
+        return consumers::run_consumer_store_transactions(settings.clone(), database.clone()).await;
+    } else if mode == "consumer_fetch_blocks" {
+        return consumers::run_consumer_fetch_blocks(settings.clone(), database.clone()).await;
     } else if mode == "consumer_assets" {
         tokio::spawn(consumers::run_consumer_fetch_assets(settings.clone(), database.clone()));
         tokio::spawn(consumers::run_consumer_fetch_assets_addresses_associations(settings.clone(), database.clone()));
