b88dac79b9802e2bbcd0d8e89de4b4c82a136c4c
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-06-24 11:16:38 +0900

    Update aerodrome.rs

--- gemstone/src/swapper/uniswap/universal_router/aerodrome.rs
+++ gemstone/src/swapper/uniswap/universal_router/aerodrome.rs
@@ -36,8 +36,4 @@ impl UniversalRouterProvider for AerodromeUniversalRouter {
     fn get_deployment_by_chain(&self, chain: &Chain) -> Option<V3Deployment> {
         get_aerodrome_router_deployment_by_chain(chain)
     }
-
-    fn use_permit2(&self) -> bool {
-        true
-    }
 }
