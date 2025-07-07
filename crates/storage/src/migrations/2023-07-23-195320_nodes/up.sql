25810b4ee6907e5de73babc0ed86f85c78f40b64
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-07-07 09:48:27 +0900

    fix diesel ci

--- crates/storage/src/migrations/2023-07-23-195320_nodes/up.sql
+++ crates/storage/src/migrations/2023-07-23-195320_nodes/up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE nodes (
   id SERIAL PRIMARY KEY,
   chain VARCHAR NOT NULL REFERENCES chains (id) ON DELETE CASCADE,
-  url VARCHAR NOT NULL,
-  node_type VARCHAR NOT NULL default 'default',
+  url VARCHAR NOT NULL,  
   status VARCHAR NOT NULL default 'active',
   priority INTEGER NOT NULL default 10,
   updated_at timestamp default current_timestamp,
-  created_at timestamp default current_timestamp
+  created_at timestamp default current_timestamp,
+  node_type VARCHAR NOT NULL default 'default'
 )
