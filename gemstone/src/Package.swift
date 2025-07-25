17c5d40b4d4663a8eb6c00df8c1ae6702beaa477
Ferman <119309671+0xh3rman@users.noreply.github.com>
2025-07-25 22:15:13 +0900

    Add macOS v15 support to Package.swift (#612)

--- gemstone/src/Package.swift
+++ gemstone/src/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Gemstone",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v17), .macOS(.v15)
     ],
     products: [
         .library(
