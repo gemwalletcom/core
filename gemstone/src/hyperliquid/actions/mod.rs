50209512bc8665db13d925f1f86cf9fe177c19dc
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-07-27 00:35:27 +0900

    implement encode methods

--- gemstone/src/hyperliquid/actions/mod.rs
+++ gemstone/src/hyperliquid/actions/mod.rs
@@ -2,12 +2,14 @@ pub mod approve_agent;
 pub mod approve_builder_fee;
 pub mod cancel_order;
 pub mod place_order;
+pub mod set_referrer;
 pub mod withdrawal;
 
 pub use approve_agent::*;
 pub use approve_builder_fee::*;
 pub use cancel_order::*;
 pub use place_order::*;
+pub use set_referrer::*;
 pub use withdrawal::*;
 
 pub const MAINNET: &str = "Mainnet";
