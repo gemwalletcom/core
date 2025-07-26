fe71c00287cb99a0d377c1e10ddef25d78c25eec
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-07-26 21:48:59 +0900

    Refactor hypercore actions into separate modules

---
+++ gemstone/src/hypercore/actions/mod.rs
@@ -0,0 +1,14 @@
+pub mod approve_agent;
+pub mod approve_builder_fee;
+pub mod cancel_order;
+pub mod place_order;
+pub mod withdrawal;
+
+pub use approve_agent::*;
+pub use approve_builder_fee::*;
+pub use cancel_order::*;
+pub use place_order::*;
+pub use withdrawal::*;
+
+pub const MAINNET: &str = "Mainnet";
+pub const SIGNATURE_CHAIN_ID: &str = "0xa4b1";
--- gemstone/src/hypercore/mod.rs
+++ gemstone/src/hypercore/mod.rs
@@ -11,15 +11,23 @@ impl HyperCore {
     }
 
     // return full eip712 message
-    fn encode_place_order(&self, _order: actions::PlaceOrder) -> String {
+    fn encode_place_order(&self, _order: actions::HyperPlaceOrder) -> String {
         todo!()
     }
 
-    fn encode_cancel_order(&self, _order: actions::CancelOrder) -> String {
+    fn encode_cancel_order(&self, _order: actions::HyperCancelOrder) -> String {
         todo!()
     }
 
-    fn encode_withdrawal_request(&self, _order: actions::WithdrawalRequest) -> String {
+    fn encode_withdrawal_request(&self, _order: actions::HyperWithdrawalRequest) -> String {
+        todo!()
+    }
+
+    fn encode_approve_agent(&self, _agent: actions::HyperApproveAgent) -> String {
+        todo!()
+    }
+
+    fn encode_approve_builder_fee(&self, _fee: actions::HyperApproveBuilderFee) -> String {
         todo!()
     }
 }
