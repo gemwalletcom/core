e2165adadf79f6a31bfa4b316fe540a900277ba1
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-08-19 09:34:52 +0900

    Update mempool.rs

--- crates/primitives/src/explorers/mempool.rs
+++ crates/primitives/src/explorers/mempool.rs
@@ -1,12 +1,12 @@
 use crate::block_explorer::BlockExplorer;
-use crate::explorers::metadata::{Explorer, Metadata};
+use crate::explorers::metadata::{Explorer, Metadata, TX_PATH, ADDRESS_PATH};
 
 pub fn new() -> Box<dyn BlockExplorer> {
     Explorer::boxed(Metadata {
         name: "Mempool",
         base_url: "https://mempool.space",
-        tx_path: "tx",
-        address_path: "address",
+        tx_path: TX_PATH,
+        address_path: ADDRESS_PATH,
         token_path: None,
         validator_path: None,
     })
