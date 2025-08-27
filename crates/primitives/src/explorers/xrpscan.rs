97bb51ec09a47754ccb010d244accddbc6d52ab7
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-08-27 15:52:39 +0900

    Update xrpscan.rs

--- crates/primitives/src/explorers/xrpscan.rs
+++ crates/primitives/src/explorers/xrpscan.rs
@@ -1,27 +1,18 @@
+use super::metadata::{Explorer, Metadata, ACCOUNT_PATH, TX_PATH};
 use crate::block_explorer::BlockExplorer;
 
 pub struct XrpScan;
 
 impl XrpScan {
     pub fn boxed() -> Box<dyn BlockExplorer> {
-        Box::new(XrpScanExplorer)
-    }
-}
-
-// Custom implementation needed because token_url uses address_path
-struct XrpScanExplorer;
-
-impl BlockExplorer for XrpScanExplorer {
-    fn name(&self) -> String {
-        "XrpScan".into()
-    }
-    fn get_tx_url(&self, hash: &str) -> String {
-        format!("https://xrpscan.com/tx/{}", hash)
-    }
-    fn get_address_url(&self, address: &str) -> String {
-        format!("https://xrpscan.com/account/{}", address)
-    }
-    fn get_token_url(&self, token: &str) -> Option<String> {
-        Some(self.get_address_url(token))
+        let config = Metadata {
+            name: "XrpScan",
+            base_url: "https://xrpscan.com",
+            tx_path: TX_PATH,
+            address_path: ACCOUNT_PATH,
+            token_path: Some(ACCOUNT_PATH),
+            validator_path: None,
+        };
+        Explorer::boxed(config)
     }
 }
