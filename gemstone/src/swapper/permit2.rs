c26f7c1da2bac12d1dce63ccb72e20da2d07ff8d
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-10-08 08:22:23 +0900

    add missing permit2_data_to_eip712_json export

--- gemstone/src/swapper/permit2.rs
+++ gemstone/src/swapper/permit2.rs
@@ -1,6 +1,10 @@
+use gem_swapper::SwapperError;
+use primitives::Chain;
+
 type Permit2Data = gem_swapper::permit2_data::Permit2Data;
 type PermitSingle = gem_swapper::permit2_data::PermitSingle;
 type Permit2Detail = gem_swapper::permit2_data::Permit2Detail;
+
 pub type Permit2ApprovalData = gem_swapper::models::Permit2ApprovalData;
 
 #[uniffi::remote(Record)]
@@ -31,4 +35,9 @@ pub struct Permit2ApprovalData {
     pub value: String,
     pub permit2_contract: String,
     pub permit2_nonce: u64,
+}
+
+#[uniffi::export]
+pub fn permit2_data_to_eip712_json(chain: Chain, data: PermitSingle, contract: &str) -> Result<String, SwapperError> {
+    gem_swapper::permit2_data::permit2_data_to_eip712_json(chain, data, contract)
 }
