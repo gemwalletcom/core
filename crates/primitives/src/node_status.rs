b8792019139f49345bfb2791872f625409d70210
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-10-20 09:51:30 +0900

    remove typeshare from NodeSyncStatus and NodeStatus

--- crates/primitives/src/node_status.rs
+++ crates/primitives/src/node_status.rs
@@ -1,9 +1,7 @@
 use crate::UInt64;
 use serde::{Deserialize, Serialize};
-use typeshare::typeshare;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[typeshare(swift = "Equatable, Hashable, Sendable")]
 #[serde(rename_all = "camelCase")]
 pub struct NodeStatus {
     pub chain_id: String,
--- crates/primitives/src/node_sync_status.rs
+++ crates/primitives/src/node_sync_status.rs
@@ -1,9 +1,7 @@
 use crate::UInt64;
 use serde::{Deserialize, Serialize};
-use typeshare::typeshare;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[typeshare(swift = "Equatable, Sendable")]
 #[serde(rename_all = "camelCase")]
 pub struct NodeSyncStatus {
     pub in_sync: bool,
