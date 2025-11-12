af80e5c39bf0063e9aebfe5478e8d6ccec2799aa
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-11-12 12:43:25 +0900

    update gradle dpes

--- gemstone/android/gemstone/build.gradle.kts
+++ gemstone/android/gemstone/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -29,11 +31,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_1_8)
+    }
+}
 
 cargoNdk {
     targets = arrayListOf("x86_64", "armeabi-v7a", "arm64-v8a")
@@ -44,12 +48,12 @@ cargoNdk {
 }
 
 dependencies {
-    api("net.java.dev.jna:jna:5.15.0@aar"){ artifact { type = "aar" } }
+    api("net.java.dev.jna:jna:5.18.1@aar"){ artifact { type = "aar" } }
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.17.0")
 
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
 
 afterEvaluate {
