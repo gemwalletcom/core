b4e295701c7ff42336431e43b75756e737e29e07
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-11-15 22:04:31 +0900

    bump cargo-ndk plugin version to 0.5.0

--- gemstone/android/build.gradle.kts
+++ gemstone/android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("com.android.application") version "8.13.1" apply false
-    id("com.gemwallet.rust.cargo-ndk-android") version "0.4.0" apply false
+    id("com.gemwallet.cargo-ndk") version "0.5.0" apply false
 }
