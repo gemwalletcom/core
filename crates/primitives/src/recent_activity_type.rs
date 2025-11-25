46b18866a3eea29e4eade1578c7f0521a5638030
gemdev1111 <iGemDev@gemwallet.com>
2025-11-25 16:48:00 +0200

    Typeshare add recent activity (#810)

---
+++ crates/primitives/src/recent_activity_type.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+use typeshare::typeshare;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[typeshare(swift = "Equatable, CaseIterable, Sendable")]
+#[serde(rename_all = "lowercase")]
+pub enum RecentActivityType {
+    Search,
+    Transfer,
+    Receive,
+}
