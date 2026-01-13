e51de52dc6baebfdb01be1da9f61fce8663bff6f
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2026-01-13 21:33:31 +0900

    publish to mvn central

--- gemstone/android/build.gradle.kts
+++ gemstone/android/build.gradle.kts
@@ -2,4 +2,16 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("com.android.application") version "8.13.1" apply false
     id("com.gemwallet.cargo-ndk") version "0.5.0" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
 }
--- gemstone/android/gemstone/build.gradle.kts
+++ gemstone/android/gemstone/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
     id("com.gemwallet.cargo-ndk")
+    id("signing")
 }
 
 android {
@@ -76,6 +77,26 @@ afterEvaluate {
                 groupId = "com.gemwallet.gemstone"
                 artifactId = "gemstone"
                 version = System.getenv("VER_NAME") ?: "1.0.0"
+                pom {
+                    name.set("Gemstone")
+                    description.set("Gem Wallet Core Android library")
+                    url.set("https://github.com/gemwalletcom/core")
+                    licenses {
+                        license {
+                            name.set("MIT")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/gemwalletcom/core")
+                    }
+                    developers {
+                        developer {
+                            id.set("gemwallet")
+                            name.set("Gem Wallet")
+                        }
+                    }
+                }
             }
             create<MavenPublication>("debug") {
                 from(components["debug"])
@@ -85,6 +106,11 @@ afterEvaluate {
             }
         }
     }
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("MVN_SIGNING_KEY"), System.getenv("MVN_SIGNING_PASSPHRASE"))
+    sign(publishing.publications["release"])
 }
 
 publishing {
