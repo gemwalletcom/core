473bf00013007eb83c50f55ab0d063862a55dd39
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2026-03-16 00:57:34 +0900

    Update secp256k1.rs

--- crates/signer/src/secp256k1.rs
+++ crates/signer/src/secp256k1.rs
@@ -2,7 +2,7 @@ use primitives::SignerError;
 use k256::ecdsa::SigningKey as SecpSigningKey;
 
 pub(crate) fn sign_digest(digest: &[u8], private_key: &[u8]) -> Result<Vec<u8>, SignerError> {
-    let signing_key = SecpSigningKey::from_slice(private_key).map_err(|_| SignerError::signing_error("Invalid Secp256k1 private key"))?;
+    let signing_key = SecpSigningKey::from_slice(private_key).map_err(|_| SignerError::invalid_input("Invalid Secp256k1 private key"))?;
     let (signature, recovery_id) = signing_key
         .sign_prehash_recoverable(digest)
         .map_err(|_| SignerError::signing_error("Failed to sign Secp256k1 digest"))?;
