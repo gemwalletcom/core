1cd5ef05e1ac68e392791fd96f01217c0534262e
gemdev111 <171273137+gemdev111@users.noreply.github.com>
2026-03-16 19:53:09 +0200

    Simplify errors

--- crates/yielder/src/error.rs
+++ crates/yielder/src/error.rs
@@ -3,42 +3,30 @@ use std::fmt::{self, Display, Formatter};
 
 use alloy_primitives::hex::FromHexError;
 use alloy_primitives::ruint::ParseError;
-use gem_client::ClientError;
-use gem_jsonrpc::types::JsonRpcError;
 
 #[derive(Debug, Clone)]
 pub enum YielderError {
-    InvalidAddress(String),
-    InvalidAmount(String),
-    UnsupportedAsset(String),
-    UnsupportedChain(String),
-    ProviderNotFound(String),
     NetworkError(String),
 }
 
 impl YielderError {
     pub fn unsupported_asset(asset: &impl Display) -> Self {
-        Self::UnsupportedAsset(asset.to_string())
+        Self::NetworkError(format!("Unsupported asset: {asset}"))
     }
 
     pub fn unsupported_chain(chain: &impl Display) -> Self {
-        Self::UnsupportedChain(chain.to_string())
+        Self::NetworkError(format!("Unsupported chain: {chain}"))
     }
 
     pub fn provider_not_found(provider: &impl Display) -> Self {
-        Self::ProviderNotFound(provider.to_string())
+        Self::NetworkError(format!("Provider not found: {provider}"))
     }
 }
 
 impl fmt::Display for YielderError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            Self::InvalidAddress(msg) => write!(f, "Invalid address: {msg}"),
-            Self::InvalidAmount(msg) => write!(f, "Invalid amount: {msg}"),
-            Self::UnsupportedAsset(msg) => write!(f, "Unsupported asset: {msg}"),
-            Self::UnsupportedChain(msg) => write!(f, "Unsupported chain: {msg}"),
-            Self::ProviderNotFound(msg) => write!(f, "Provider not found: {msg}"),
-            Self::NetworkError(msg) => write!(f, "Network error: {msg}"),
+            Self::NetworkError(msg) => write!(f, "{msg}"),
         }
     }
 }
@@ -47,24 +35,12 @@ impl Error for YielderError {}
 
 impl From<FromHexError> for YielderError {
     fn from(err: FromHexError) -> Self {
-        Self::InvalidAddress(err.to_string())
+        Self::NetworkError(err.to_string())
     }
 }
 
 impl From<ParseError> for YielderError {
     fn from(err: ParseError) -> Self {
-        Self::InvalidAmount(err.to_string())
-    }
-}
-
-impl From<ClientError> for YielderError {
-    fn from(err: ClientError) -> Self {
-        Self::NetworkError(err.to_string())
-    }
-}
-
-impl From<JsonRpcError> for YielderError {
-    fn from(err: JsonRpcError) -> Self {
         Self::NetworkError(err.to_string())
     }
 }
@@ -77,6 +53,6 @@ impl From<Box<dyn std::error::Error + Send + Sync>> for YielderError {
 
 impl From<strum::ParseError> for YielderError {
     fn from(err: strum::ParseError) -> Self {
-        Self::ProviderNotFound(err.to_string())
+        Self::NetworkError(err.to_string())
     }
 }
