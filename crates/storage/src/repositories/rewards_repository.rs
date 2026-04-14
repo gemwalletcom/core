19f625dd033c99b5f67494482400c0cdb8506ecb
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-04-14 23:59:37 +0000

    Return redemption options for all rewards statuses

--- crates/storage/src/repositories/rewards_repository.rs
+++ crates/storage/src/repositories/rewards_repository.rs
@@ -234,15 +234,11 @@ impl RewardsRepository for DatabaseClient {
         let code = if has_custom_code { Some(username.username.clone()) } else { None };
 
         let status = *rewards.status;
-        let options = if status.is_verified() {
-            let types = [RewardRedemptionType::Asset];
-            RewardsRedemptionsRepository::get_redemption_options(self, &types)?
-                .into_iter()
-                .filter(|x| x.remaining.unwrap_or_default() > 0)
-                .collect()
-        } else {
-            vec![]
-        };
+        let types = [RewardRedemptionType::Asset];
+        let options = RewardsRedemptionsRepository::get_redemption_options(self, &types)?
+            .into_iter()
+            .filter(|x| x.remaining.unwrap_or_default() > 0)
+            .collect();
 
         Ok(Rewards {
             code,
