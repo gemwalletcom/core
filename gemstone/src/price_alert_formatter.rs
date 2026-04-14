a806f26b0375c67356398bf182e4dedf0d0ecd9b
Radmir <radmir@gemwallet.com>
2026-04-14 20:59:04 +0500

    Wrap price alert functions in PriceAlertFormatter struct

--- gemstone/src/price_alert_formatter.rs
+++ gemstone/src/price_alert_formatter.rs
@@ -1,11 +1,20 @@
 use number_formatter::price_suggestion;
 
-#[uniffi::export]
-pub fn price_alert_percentage_suggestions(price: f64) -> Vec<i32> {
-    price_suggestion::percentage_suggestions(price)
-}
+#[derive(Default, uniffi::Object)]
+pub struct PriceAlertFormatter {}
 
 #[uniffi::export]
-pub fn price_alert_rounded_values(price: f64, by_percent: f64) -> Vec<f64> {
-    price_suggestion::price_rounded_values(price, by_percent)
+impl PriceAlertFormatter {
+    #[uniffi::constructor]
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn percentage_suggestions(&self, price: f64) -> Vec<i32> {
+        price_suggestion::percentage_suggestions(price)
+    }
+
+    pub fn rounded_values(&self, price: f64, by_percent: f64) -> Vec<f64> {
+        price_suggestion::price_rounded_values(price, by_percent)
+    }
 }
